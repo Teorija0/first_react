0738121967f0f731a7cecc381f1f65ae588b934d
Teorija <teodor.blok@gmail.com>
2024-10-26 19:02:30 +0200

    delete commit, add page UnlimitedVoid

---
+++ src/UnlimitedVoid.jsx
@@ -0,0 +1,27 @@
+const UnlimitedVoid = () => {
+  return (
+    <div>
+      <h1>Unlimited Void</h1>
+
+      <p>
+        Unlimited Void: Satoru Gojo, a prominent character in Jujutsu Kaisen,
+        possesses one of the most potent Domain Expansions known as
+        &quot;Unlimited Void.&quot; This exceptional technique immerses both the
+        user and the target inside the Limitless itself—a vast void filled with
+        overwhelming information. The sheer volume of information incapacitates
+        the target, rendering them immobile and defenseless.
+      </p>
+      <p>
+        Unparalleled Power: &quot;Unlimited Void&quot; stands as one of the most
+        abstract and powerful domains in the series. The transcendental nature
+        of this expansion showcases Gojo&apos;s omnipotence, making it
+        challenging for adversaries to contend with the sheer force and
+        complexity of the domain. Notably, Gojo is one of the few individuals
+        capable of using Domain Expansion multiple times in a day, highlighting
+        the rarity and mastery required for such a feat.
+      </p>
+    </div>
+  );
+};
+
+export default UnlimitedVoid;
